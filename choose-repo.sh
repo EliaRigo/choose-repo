9f802d12a697ca63728a2467b5404b383844b4f0
EliaRigo <elia.rigo@gmail.com>
2013-12-09 15:19:21 +0100

    clean project

--- choose-repo.sh
+++ choose-repo.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-red='\e[031m'
-blue='\e[034m'
-NC='\e[0m' # No Color
 
 cd user_path
 
@@ -21,14 +18,10 @@ let i++
 done
 
 read -p "Repo: " r
-#echo "$r: ${array[$r]}"
 cd ${array[$r]}
 
 while true; do 
     clear
-    #git log --reverse --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-    #git log --graph --topo-order --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
-	#git lg
 	#
 	# INSERT HERE YOUR CUSTOM git log
 	#
