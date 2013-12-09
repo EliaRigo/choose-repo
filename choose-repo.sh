b0ba2c3c912ff66d55eeae71f4513a4463dab864
EliaRigo <elia.rigo@gmail.com>
2013-12-09 14:51:32 +0100

    Add user_path and custom git log

--- choose-repo.sh
+++ choose-repo.sh
@@ -3,7 +3,7 @@ red='\e[031m'
 blue='\e[034m'
 NC='\e[0m' # No Color
 
-cd /d/Users/eliar_000/Documents/Visual.Studio.2013/Projects/
+cd user_path
 
 array=(`ls -1 --ignore="*.*"`)
 echo $array
@@ -27,8 +27,11 @@ cd ${array[$r]}
 while true; do 
     clear
     #git log --reverse --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-    git log --graph --topo-order --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
+    #git log --graph --topo-order --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
 	#git lg
+	#
+	# INSERT HERE YOUR CUSTOM git log
+	#
 	read -p "" qe
 	case $qe in
         [QqEe]* ) clear; exit;;
