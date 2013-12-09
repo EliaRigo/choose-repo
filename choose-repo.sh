49c01ce01fef439f91705c315f99cb2066666123
EliaRigo <elia.rigo@gmail.com>
2013-12-09 14:46:19 +0100

    first commit

---
+++ choose-repo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+red='\e[031m'
+blue='\e[034m'
+NC='\e[0m' # No Color
+
+cd /d/Users/eliar_000/Documents/Visual.Studio.2013/Projects/
+
+array=(`ls -1 --ignore="*.*"`)
+echo $array
+len=${#array[*]}
+
+i=0
+while [ $i -lt $len ]; do
+if (($i%2 == 0))
+then
+	echo -e "${red} $i) ${array[$i]}${NC}"
+	else
+		echo -e "${blue} $i) ${array[$i]}${NC}"
+fi
+let i++
+done
+
+read -p "Repo: " r
+#echo "$r: ${array[$r]}"
+cd ${array[$r]}
+
+while true; do 
+    clear
+    #git log --reverse --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+    git log --graph --topo-order --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
+	#git lg
+	read -p "" qe
+	case $qe in
+        [QqEe]* ) clear; exit;;
+	esac
+	sleep 5
+done
